229757a6970320d60c04d946975a3103fd5fd983
ranoji <33272819+ranoji@users.noreply.github.com>
2024-07-19 16:34:04 +0530

    Update NineDotsMenu.jsx

--- NineDotsMenu.jsx
+++ NineDotsMenu.jsx
@@ -1,46 +1,12 @@
-// NineDotsMenu.js
+const handleBlankAreaClick = () => {
+    if (gridRef.current) {
+      gridRef.current.api.deselectAll();
+      gridRef.current.api.setFocusedCell(0, 'name');
+    }
+  };
 
-import React from "react";
-import Box from "@mui/material/Box";
-import { styled } from "@mui/system";
-
-const Dot = styled(Box)({
-  flex: "0 0 calc(33.33% - 4px)", // Adjust width to maintain 9x9 box ratio and reduce spacing
-  height: "0",
-  paddingBottom: "calc(33.33% - 4px)",
-  backgroundColor: "blue",
-  margin: "1px", // Adjust spacing between dots
-});
-
-const Circle = styled(Box)({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "50px",
-  height: "50px",
-  backgroundColor: "lightgrey",
-  borderRadius: "50%",
-  cursor: "pointer",
-  padding: "4px",
-  boxSizing: "border-box",
-});
-
-const NineDotsMenu = ({ toggleBox }) => {
   return (
-    <Circle onClick={toggleBox}>
-      <Box
-        display="flex"
-        flexWrap="wrap"
-        justifyContent="center"
-        alignItems="center"
-        width="100%"
-      >
-        {[...Array(9)].map((_, index) => (
-          <Dot key={index} />
-        ))}
-      </Box>
-    </Circle>
-  );
-};
-
-export default NineDotsMenu;
+    <div
+      style={{ width: '100%', height: '100%' }}
+      onClick={handleBlankAreaClick}
+    >
