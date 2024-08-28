de97d466fb228afa41a4daa5448afc8552ce9b44
ranoji <33272819+ranoji@users.noreply.github.com>
2024-08-29 00:04:50 +0530

    Update NineDotsMenu.jsx

--- NineDotsMenu.jsx
+++ NineDotsMenu.jsx
@@ -1,7 +1,20 @@
  positioner: function (labelWidth, labelHeight, point) {
+      const chartWidth = this.chart.chartWidth;
       let tooltipX, tooltipY;
+
       tooltipX = point.plotX + this.chart.plotLeft + 10;
       tooltipY = point.plotY + this.chart.plotTop - 10;
+
+      // Adjust if tooltip goes beyond the right boundary
+      if (tooltipX + labelWidth > chartWidth) {
+        tooltipX -= (tooltipX + labelWidth) - chartWidth + 10;
+      }
+
+      // Prevent tooltip from going outside the left boundary
+      if (tooltipX < 0) {
+        tooltipX = 10;
+      }
+
       return {
         x: tooltipX,
         y: tooltipY,
